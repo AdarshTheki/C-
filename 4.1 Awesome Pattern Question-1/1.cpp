375f72b51a09e1899da1f855c794f3d78144caa3
Adarsh Verma <adarshverma549@gmail.com>
2023-01-29 08:27:17 +0530

    '*' print method to 1000+, how?

--- 4.1 Awesome Pattern Question-1/1.cpp
+++ 4.1 Awesome Pattern Question-1/1.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 int main(){
-
+    // Create inputs of how manys rows and columns are there.
     int row, col;
     cin>>row>>col;
 
+    // How many rows are there and outer loop will run 
     for(int i=1; i<=row; i++){
+
+    // How many columns and which type to print.
         for(int j=1; j<=col; j++){
-            cout<<"* ";
+            cout<<"* ";     // What do you need to print
         }
-        cout<<endl;
+        cout<<endl;         
     }
     return 0;
 }
+// This methd tearn to 1000+ star pattern you will create, Please follow this method. 
